a6b2b5d595f107f1d38c995b54448a8d5ed6341d
Hannah Willis <han.e.willis@gmail.com>
2018-12-24 08:20:31 -0600

    updated

---
+++ app/routing/connection.js
@@ -0,0 +1,14 @@
+var myswl = require("mysql");
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "hacktheplanet",
+    database: "todoagain_db"
+  });
+}
